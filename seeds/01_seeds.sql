fd1d279fdb1dc92d2f47cc4ffe876fde754e57c1
Alex <your@email.com>
2022-03-10 20:45:38 +0000

    created inital queries (1_queries)

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (name, email, password) 
+VALUES (Tommy Wiseau, tw@theroom.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.)
+INSERT INTO users (name, email, password) 
+VALUES (Keanu Reeves, kr@matrix.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.)
+INSERT INTO users (name, email, password) 
+VALUES (Justin Bieber, jb@beibs.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.)
+INSERT INTO users (name, email, password) 
+VALUES (Jim Carrey, jc@carrey.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.)
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, city, province, post_code, active)
